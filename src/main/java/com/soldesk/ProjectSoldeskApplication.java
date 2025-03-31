0dfc08426ba58fd62e4101d13e146f44098a7b3b
Utae Kim <raldnxo90@gmial.com>
2025-03-31 12:48:22 +0900

    # first commit

---
+++ src/main/java/com/soldesk/ProjectSoldeskApplication.java
@@ -0,0 +1,13 @@
+package com.soldesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSoldeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSoldeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soldesk/ProjectSoldeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soldesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSoldeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
